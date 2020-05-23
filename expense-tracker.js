439673520d11c88df0457eadbd30e07f9685a9e2
sigitp-git <sigitp@amazon.com>
2020-05-23 15:32:52 -0500

    challenge using reduce on array of objects

--- expense-tracker.js
+++ expense-tracker.js
@@ -2,14 +2,25 @@ const account = {
   name: "Mici Bici",
   expenses: [],
   addExpense: function (desc, amt) {
-    this.expenses.push({ description: desc, amount: amt })
+    this.expenses.push({ ex_description: desc, amount: amt })
     return console.log(this.expenses)
   },
-  getAccountSummary: function () {
-    const amtsum = this.expenses.reduce((acc, cur) => ({
+  getExpenseSummary: function () {
+    const expSum = this.expenses.reduce((acc, cur) => ({
       amount: acc.amount + cur.amount,
     }))
-    console.log(amtsum.amount)
+    return expSum.amount
+  },
+  income: [],
+  addIncome: function (desc, amt) {
+    this.income.push({ in_description: desc, amount: amt })
+    return console.log(this.income)
+  },
+  getIncomeSummary: function () {
+    const incSum = this.income.reduce((acc, cur) => ({
+      amount: acc.amount + cur.amount,
+    }))
+    return incSum.amount
   },
 }
 
@@ -18,5 +29,14 @@ account.addExpense("Coffee", 10)
 account.addExpense("Coffee", 10)
 account.addExpense("Keyboard", 200)
 account.addExpense("Mouse", 100)
-account.getAccountSummary()
-console.log(account)
+
+account.addIncome("Coding", 1500)
+account.addIncome("Cloud", 10000)
+
+console.log(
+  `${
+    account.name
+  } has $${account.getIncomeSummary()} income and $${account.getExpenseSummary()} expenses. Total account now $${
+    account.getIncomeSummary() - account.getExpenseSummary()
+  } available.`
+)
