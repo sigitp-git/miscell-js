addf7d6879b3a3e72f43383d12c40ee1ce0d603f
sigitp-git <sigitp@amazon.com>
2020-06-06 18:50:20 -0500

    rest operator

--- restoperator/index.js
+++ restoperator/index.js
@@ -1,7 +1,18 @@
-const avg = (...numbers) => {
-    let sum = 0
-    numbers.map((num) => sum += num)
-    return sum/numbers.length
+const avg = (thing1, thing2, ...numbers) => {
+  let sum = 0
+  numbers.map((num) => (sum += num))
+  const average = sum / numbers.length
+  return `The average ${thing1} is ${average} ${thing2}`
 }
 
-console.log(avg(100,100,100,100,100,100))
+console.log(avg('temp', 'Fahrenheit', 100, 100, 100, 100, 100, 100))
+
+const printTeam = (thing1, thing2, ...cats) => {
+    let string = `Team "${thing1}" with Coach "${thing2}". Members are: `
+    cats.map((cat) => {
+        string += `${cat}, `
+    })
+    return console.log(string)
+}
+
+printTeam('Team Cat', 'Coach Cat', 'Bici', 'Mici', 'Kong Bu', 'Ku Bu')
